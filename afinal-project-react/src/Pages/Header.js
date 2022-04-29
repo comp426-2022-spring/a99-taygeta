399b5c13e6de088033151ea7942ced94a4c11877
Raven Taylor <raven61@live..unc.edu>
2022-04-29 18:00:58 -0400

    finished backend

--- afinal-project-react/src/Pages/Header.js
+++ afinal-project-react/src/Pages/Header.js
@@ -2,6 +2,11 @@ function Header() {
   return (
     <div className="Header">
       <h1>UNC Mental Health</h1>
+      <p>
+        Don't forget to start the server! The pages will not work properly
+        without it.
+      </p>
+      <p>areact-final-project/backend/server.js</p>
     </div>
   );
 }
--- afinal-project-react/src/Pages/Login.js
+++ afinal-project-react/src/Pages/Login.js
@@ -1,59 +1,64 @@
 import "../css/Login.css";
 import axios from "axios";
+
 function Login() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const email1 = e.target.email.value;
+    axios
+      .post("http://localhost:5555/app/email/", { email: email1 })
+      .then((res) => {
+        console.log(res.data);
+        if (res.data == "0") {
+          alert(
+            "There's no record of this email. Create a new account instead."
+          );
+          window.location.href = "/register";
+        } else {
+          alert("We found your email! Let's take you to the dashboard.");
+          window.location.href = "/dashboard";
+        }
+      });
+  };
   return (
-    <div className="App">
+    <form onSubmit={onSubmit}>
       <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-xl-10 col-lg-12 col-md-9">
-            <div className="card o-hidden border-0 shadow-lg my-5">
-              <div className="card-body p-0">
-                <div className="row">
-                  <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                  <div className="col-lg-6">
-                    <div className="p-5">
-                      <div className="text-center">
-                        <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                      </div>
-                      <form className="user">
-                        <div className="form-group">
-                          <input
-                            type="email"
-                            className="form-control form-control-user"
-                            id="exampleInputEmail"
-                            aria-describedby="emailHelp"
-                            placeholder="Enter Email Address..."
-                          ></input>
-                        </div>
-                        <div className="form-group">
-                          <input
-                            type="password"
-                            className="form-control form-control-user"
-                            id="exampleInputPassword"
-                            placeholder="Password"
-                          ></input>
-                        </div>
-                        <a
-                          href="dashboard/"
-                          className="btn btn-primary btn-user btn-block"
-                        >
-                          Login
-                        </a>
-                      </form>
-                      <div className="text-center">
-                        <a className="small" href="/register">
-                          Create an Account!
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <label htmlFor="uname">
+          <b>Username</b>
+        </label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Enter Username"
+          required
+        ></input>
+
+        <label htmlFor="psw">
+          <b>Password</b>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="psw"
+          required
+        ></input>
+
+        <button type="submit">Login</button>
+        <label>
+          <input
+            type="checkbox"
+            defaultChecked={true}
+            name="remember"
+            style={{ marginBottom: "15px" }}
+          ></input>{" "}
+          Remember me
+        </label>
       </div>
-    </div>
+
+      <div className="container">
+        <button className="cancelbtn">Register</button>
+      </div>
+    </form>
   );
 }
 
